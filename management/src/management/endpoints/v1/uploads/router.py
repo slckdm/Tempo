70f7950925c12e9d9e0e1842054b044ab530a309
Dmitry <zubashenkod@gmail.com>
2026-06-10 18:16:49 +0300

    chore: update uploads endpoint router

--- management/src/management/endpoints/v1/uploads/router.py
+++ management/src/management/endpoints/v1/uploads/router.py
@@ -1,8 +1,17 @@
 """Module: uploads endpoint router."""
 
+from http import HTTPStatus
+
 from fastapi import APIRouter
 
-from toolkit.response import JSendErrorResponse, JSendFailResponse, JSendSuccessfulResponse
+from toolkit.service.response import (
+    BadRequestResponse,
+    ForbiddenResponse,
+    InternalServerErrorResponse,
+    JSendSuccessfulResponse,
+    UnsupportedMediaTypeResponse,
+    ValidationErrorResponse,
+)
 
 from management.schemas.response import CreateUploadResponseBody
 
@@ -11,8 +20,16 @@ from .create_upload import create_upload
 
 uploads_router = APIRouter(prefix="/uploads", tags=["Uploads"])
 common_responses = {
-    400: {"model": JSendFailResponse, "description": "Bad request"},
-    422: {"model": JSendErrorResponse, "description": "Validation error"},
+    HTTPStatus.BAD_REQUEST: {"model": BadRequestResponse, "description": "Bad request"},
+    HTTPStatus.FORBIDDEN: {"model": ForbiddenResponse, "description": "Forbidden"},
+    HTTPStatus.UNPROCESSABLE_CONTENT: {
+        "model": ValidationErrorResponse,
+        "description": "Validation error",
+    },
+    HTTPStatus.INTERNAL_SERVER_ERROR: {
+        "model": InternalServerErrorResponse,
+        "description": "Internal server error",
+    },
 }
 
 uploads_router.add_api_route(
@@ -20,7 +37,13 @@ uploads_router.add_api_route(
     create_upload,
     methods=["POST"],
     response_model=JSendSuccessfulResponse[CreateUploadResponseBody],
-    responses={**common_responses},
+    responses={
+        **common_responses,
+        HTTPStatus.UNSUPPORTED_MEDIA_TYPE: {
+            "model": UnsupportedMediaTypeResponse,
+            "description": "Unsupported media type error",
+        },
+    },
 )
 
 # uploads_router.add_api_route(
