58263f288afdda77b86605143391b030a118775b
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:15:40 +0300

    chore: move schemas

--- management/src/app/schemas/response/complete_upload_response_body.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class CompleteUploadResponseBody(BaseModel):
-
-    urn: str = Field(
-        description="The URN of the completed upload.",
-        json_schema_extra={"example": "urn:upload:123"}
-    )
--- management/src/app/schemas/types_.py
+++
@@ -1,7 +0,0 @@
-from toolkit.types_ import URNType
-
-
-class UploadURNType(URNType):
-    """Upload URN type."""
-
-    namespace = "mng.upload"
