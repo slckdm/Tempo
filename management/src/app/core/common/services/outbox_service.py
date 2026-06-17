fb8547837b69797f9038925f79978564f083265e
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:23:10 +0300

    chore: update import

--- management/src/app/core/common/services/outbox_service.py
+++ management/src/app/core/common/services/outbox_service.py
@@ -2,7 +2,7 @@ from dishka import Provider
 
 from app.core.common.enums import AggregateType, EventType
 from app.core.models import OutboxMessage
-from app.core.ports.system_utc_timer import UTCTimer
+from app.core.ports.utc_timer import UTCTimer
 
 
 class OutboxService(Provider):
