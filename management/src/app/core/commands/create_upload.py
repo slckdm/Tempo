a6fe939e67f5473ae3c59491648f6ba18e851e2a
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:13:12 +0300

    chore:  update create upload command

--- management/src/app/core/commands/create_upload.py
+++ management/src/app/core/commands/create_upload.py
@@ -1,15 +1,16 @@
 from toolkit.s3 import S3Client
 from toolkit.service.response import JSendSuccessfulResponse
 
+from app.core.common.enums import AggregateType, EventType
+from app.core.common.services import CurrentUserService, OutboxService, UploadService
 from app.core.ports.flusher import Flusher
+from app.core.ports.outbox_storage import OutboxStorage
 from app.core.ports.transaction import Transaction
 from app.core.ports.upload_storage import UploadStorage
+from app.core.schemas.dto import UploadDTO
+from app.core.schemas.request import CreateUploadRequestBody
+from app.core.schemas.response import CreateUploadResponseBody
 from app.main.config.settings import S3Settings
-from app.schemas.dto import UploadDTO
-from app.schemas.request import CreateUploadRequestBody
-from app.schemas.response import CreateUploadResponseBody
-from app.services.current_user_service import CurrentUserService
-from app.services.upload_service import UploadService
 
 
 class CreateUpload:
@@ -17,33 +18,43 @@ class CreateUpload:
         self,
         current_user_service: CurrentUserService,
         upload_service: UploadService,
+        upload_storage: UploadStorage,
+        outbox_service: OutboxService,
+        outbox_storage: OutboxStorage,
         s3: S3Client,
         s3_config: S3Settings,
         flusher: Flusher,
         transaction: Transaction,
-        upload_storage: UploadStorage,
     ) -> None:
         self._current_user_service = current_user_service
         self._upload_service = upload_service
+        self._upload_storage = upload_storage
+        self._outbox_service = outbox_service
+        self._outbox_storage = outbox_storage
         self._s3 = s3
         self._s3_config = s3_config
         self._flusher = flusher
         self._transaction = transaction
-        self._upload_storage = upload_storage
 
     async def execute(
         self,
         body: CreateUploadRequestBody,
     ) -> JSendSuccessfulResponse[CreateUploadResponseBody]:
         user = await self._current_user_service.get_current_user()
-        presigned_url = self._s3.generate_presigned_url(
-            self._s3_config.BUCKET, body.filename, content_type=body.content_type
-        )
-        upload = await self._upload_service.create_upload(
-            body.filename, body.content_type, body.size, user
-        )
+        upload = await self._upload_service.create_upload(body.filename, body.size, user)
         await self._upload_storage.add(upload)
         await self._flusher.flush([upload])
+        presigned_url = self._s3.generate_presigned_url(
+            self._s3_config.BUCKET, str(upload.urn), content_type=body.content_type
+        )
+        message = await self._outbox_service.create_message(
+            aggregate_type=AggregateType.UPLOAD,
+            aggregate_id=str(upload.id),
+            event_type=EventType.UPLOAD_CREATED,
+            payload={},
+        )
+        await self._outbox_storage.add(message)
+
         await self._transaction.commit()
 
         return JSendSuccessfulResponse(
