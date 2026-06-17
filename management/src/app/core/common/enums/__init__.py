5917e7616f18787abc63a6f7be681aa083f58eba
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:13:43 +0300

    chore: add event type and aggregate type enumerations

--- management/src/app/core/common/enums/__init__.py
+++ management/src/app/core/common/enums/__init__.py
@@ -1,5 +1,11 @@
 """Package: common enumerations."""
 
+from .aggregate_type import AggregateType
+from .event_type import EventType
 from .upload_status import UploadStatus
 
-__all__ = ["UploadStatus"]
+__all__ = [
+    "UploadStatus",
+    "AggregateType",
+    "EventType",
+]
---
+++ management/src/app/core/common/enums/aggregate_type.py
@@ -0,0 +1,6 @@
+from enum import StrEnum
+
+
+class AggregateType(StrEnum):
+
+    UPLOAD = "upload"
