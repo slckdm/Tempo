92d56377a08efedebbfa82ae15f29791c5638aab
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:22:46 +0300

    chore: update upload storage implementation

--- management/src/app/outbound/adapters/sqla_upload_storage.py
+++ management/src/app/outbound/adapters/sqla_upload_storage.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.models import Upload
@@ -8,8 +10,8 @@ class SQLAUploadStorage(UploadStorage):
     def __init__(self, session: AsyncSession) -> None:
         self.__session = session
 
-    async def add(self, upload: Upload):
+    async def add(self, upload: Upload) -> None:
         self.__session.add(upload)
 
-    async def get_by_id(self, id: int, for_update: bool) -> Upload | None:
+    async def get_by_id(self, id: UUID, for_update: bool = False) -> Upload | None:
         return await self.__session.get(Upload, id, with_for_update=for_update)
