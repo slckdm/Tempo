cd4739570faaa865a2168c61b30ce5b1166cf6cd
Dmitry <zubashenkod@gmail.com>
2026-06-17 13:18:47 +0300

    chore: register providers

--- management/src/app/main/ioc/core.py
+++ management/src/app/main/ioc/core.py
@@ -1,20 +1,26 @@
 from dishka import Provider, Scope, provide
 
+from app.core.commands.complete_upload import CompleteUpload
 from app.core.commands.create_upload import CreateUpload
-from app.core.ports.flusher import Flusher
-from app.core.ports.transaction import Transaction
-from app.core.ports.upload_storage import UploadStorage
-from app.outbound.adapters.sqla_flusher import SQLAFlusher
-from app.outbound.adapters.sqla_transaction import SQLATransaction
-from app.outbound.adapters.sqla_upload_storage import SQLAUploadStorage
-from app.outbound.keycloak_auth_user_finder import KeycloakAuthorizedUserFinder
-from app.outbound.keycloak_identity_provider import KeycloakIdentityProvider
-from app.services.current_user_service import (
+from app.core.common.services.current_user_service import (
     AuthorizedUserFinder,
     CurrentUserService,
     IdentityProvider,
 )
-from app.services.upload_service import UploadService
+from app.core.common.services.outbox_service import OutboxService
+from app.core.common.services.upload_service import UploadService
+from app.core.ports.flusher import Flusher
+from app.core.ports.outbox_storage import OutboxStorage
+from app.core.ports.system_utc_timer import UTCTimer
+from app.core.ports.transaction import Transaction
+from app.core.ports.upload_storage import UploadStorage
+from app.outbound.adapters.sqla_flusher import SQLAFlusher
+from app.outbound.adapters.sqla_outbox_storage import SQLAOutboxStorage
+from app.outbound.adapters.sqla_transaction import SQLATransaction
+from app.outbound.adapters.sqla_upload_storage import SQLAUploadStorage
+from app.outbound.adapters.system_utc_timer import SystemUTCTimer
+from app.outbound.keycloak_auth_user_finder import KeycloakAuthorizedUserFinder
+from app.outbound.keycloak_identity_provider import KeycloakIdentityProvider
 
 
 class CoreProvider(Provider):
@@ -27,11 +33,15 @@ class CoreProvider(Provider):
     # Services
     current_user_service = provide(CurrentUserService)
     upload_service = provide(UploadService)
+    outbox_service = provide(OutboxService)
 
     # ports
     flusher = provide(SQLAFlusher, provides=Flusher)
     transaction = provide(SQLATransaction, provides=Transaction)
     upload_storage = provide(SQLAUploadStorage, provides=UploadStorage)
+    outbox_storage = provide(SQLAOutboxStorage, provides=OutboxStorage)
+    utc_timer = provide(SystemUTCTimer, provides=UTCTimer)
 
     # commands
     create_upload = provide(CreateUpload)
+    complete_upload = provide(CompleteUpload)
