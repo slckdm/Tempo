b175e64e1f9a76145e47541ff27728b3b2daf1db
Dmitry <zubashenkod@gmail.com>
2026-06-23 20:06:58 +0300

    chore: wrap s3 response into object entity

--- toolkit/toolkit/s3/s3_client.py
+++ toolkit/toolkit/s3/s3_client.py
@@ -1,6 +1,10 @@
+import asyncio
+
 from boto3.session import Session
 from botocore.config import Config
-from mypy_boto3_s3.type_defs import GetObjectOutputTypeDef
+from botocore.exceptions import ClientError
+
+from toolkit.entities.object import Object
 
 
 class NoSuchKeyException(Exception):
@@ -14,25 +18,25 @@ class S3Client:
         self, url: str, access_key_id: str, secret_access_key: str, region_name: None | str = None
     ) -> None:
         """Initialize S3 API client."""
-        self.__session = Session(
+        self._session = Session(
             aws_access_key_id=access_key_id,
             aws_secret_access_key=secret_access_key,
             region_name=region_name,
         )
-        self.__client = self.__session.client(
+        self._client = self._session.client(
             service_name="s3",
             endpoint_url=url,
             config=Config(signature_version="s3v4"),
         )
 
-    def get_object(self, bucket_name: str, key: str, **kwargs) -> GetObjectOutputTypeDef | None:
+    async def get_object(self, bucket: str, key: str, **kwargs) -> Object:
         """Get existing object."""
         try:
-            return self.__client.get_object(
-                Bucket=bucket_name,
-                Key=key,
-                **kwargs
+            response_data = await asyncio.to_thread(
+                self._client.get_object, Bucket=bucket, Key=key, **kwargs
             )
+            return Object.model_validate(response_data)
+
         except Exception as exc:
             if "NoSuchKey" in str(exc):
                 raise NoSuchKeyException from exc
@@ -41,17 +45,31 @@ class S3Client:
 
     def generate_presigned_url(
         self,
-        bucket_name: str,
-        object_name: str,
+        bucket: str,
+        key: str,
         content_type: None | str = None,
         expiration: int = 3600,
     ) -> str:
         """Generate presigned url for creating a new object."""
-        params = {"Bucket": bucket_name, "Key": object_name}
+        params = {"Bucket": bucket, "Key": key}
         if content_type is not None:
             params["ContentType"] = content_type
-        return self.__client.generate_presigned_url(
+        return self._client.generate_presigned_url(
             "put_object",
             Params=params,
             ExpiresIn=expiration,
         )
+
+    async def put_object(
+        self, bucket: str, key: str, body: bytes, **kwargs
+    ) -> None:
+        try:
+            await asyncio.to_thread(
+                self._client.put_object,
+                Bucket=bucket,
+                Key=key,
+                Body=body,
+                **kwargs,
+            )
+        except ClientError as client_err:
+            raise client_err
