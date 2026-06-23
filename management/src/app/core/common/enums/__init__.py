93737936d40c3d434c37e08ea5b4b267440a6773
Dmitry <zubashenkod@gmail.com>
2026-06-23 20:12:31 +0300

    chore: remove enums

--- management/src/app/core/common/enums/__init__.py
+++ management/src/app/core/common/enums/__init__.py
@@ -1,11 +1,7 @@
 """Package: common enumerations."""
 
 from .aggregate_type import AggregateType
-from .event_type import EventType
-from .upload_status import UploadStatus
 
 __all__ = [
-    "UploadStatus",
     "AggregateType",
-    "EventType",
 ]
--- management/src/app/core/common/enums/event_type.py
+++
@@ -1,8 +0,0 @@
-from enum import StrEnum
-
-from app.core.common.types_ import UploadURNType
-
-
-class EventType(StrEnum):
-    UPLOAD_CREATED = f"event:{UploadURNType.namespace}:created"
-    UPLOAD_COMPLETED = f"event:{UploadURNType.namespace}:completed"
