a4c1c59459048fa79956b288269cedeedc6d6ba1
Dmitry <zubashenkod@gmail.com>
2026-06-23 20:07:20 +0300

    chore: add custom exception

--- toolkit/toolkit/service/exceptions.py
+++ toolkit/toolkit/service/exceptions.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
 
 
-class UnauthorizedException(Exception):
-    ...
+class TempoException(Exception):
+    status_code: HTTPStatus | int
+    detail: str
+    data: dict
+
+    def __init__(self, detail: str | None = None, data: dict | None = None) -> None:
+        self.detail = detail or self.__class__.detail
+        self.data = data or {}
 
 
-class ForbiddenException(Exception):
-    ...
+class UnauthorizedException(TempoException):
+    status_code = HTTPStatus.UNAUTHORIZED
+    detail = HTTPStatus.UNAUTHORIZED.phrase
+
+
+class ForbiddenException(TempoException):
+    status_code = HTTPStatus.FORBIDDEN
+    detail = HTTPStatus.FORBIDDEN.phrase
+
+
+class NotFoundException(TempoException):
+    status_code = HTTPStatus.NOT_FOUND
+    detail = HTTPStatus.NOT_FOUND.phrase
