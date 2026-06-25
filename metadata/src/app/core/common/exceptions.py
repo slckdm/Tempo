bd7f256d5ed90999493b546a1b2285e33e57738b
Dmitry <zubashenkod@gmail.com>
2026-06-25 16:59:07 +0300

    chore: update imports

--- metadata/src/app/core/common/exceptions.py
+++ metadata/src/app/core/common/exceptions.py
@@ -1,20 +1,20 @@
 from http import HTTPStatus
 
-from toolkit.service.exceptions import TempoException
+from toolkit.service.exceptions import TempoBaseException
 
 
-class BaseError(TempoException):
+class BaseError(TempoBaseException):
     default_message: str | None = None
 
     def __init__(self, message: str | None = None) -> None:
         super().__init__(message or self.default_message)
 
 
-class MetadataAlreadyProcessed(TempoException):
+class MetadataAlreadyProcessed(TempoBaseException):
     status_code = HTTPStatus.FORBIDDEN
     detail = "Metadata has been already processed."
 
 
-class TagParseError(TempoException):
+class TagParseError(TempoBaseException):
     status_code = 500
     detail = "Cannot parse tags."
