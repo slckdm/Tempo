9c32a1de8f82ea73e3064eca308248b69ae1e01f
Dmitry <zubashenkod@gmail.com>
2026-06-25 17:18:01 +0300

    chore: update imports

--- streaming/src/app/main/ioc/core.py
+++ streaming/src/app/main/ioc/core.py
@@ -1,8 +1,7 @@
 from dishka import Provider, Scope, provide
 
-from app.core.commands.ports.object_storage import ObjectStorage
-from app.core.commands.stream_audio import StreamAudio
-from app.core.commands.stream_cover import StreamCover
+from app.core.queries.ports.object_storage import ObjectStorage
+from app.core.queries.stream import Stream
 from app.outbound.adapters.s3_object_storage import S3ObjectStorage
 from app.outbound.keycloak_auth_user_finder import KeycloakAuthorizedUserFinder
 from app.outbound.keycloak_identity_provider import KeycloakIdentityProvider
@@ -23,5 +22,4 @@ class CoreProvider(Provider):
     object_storage = provide(S3ObjectStorage, provides=ObjectStorage)
 
     # commands
-    stream_audio = provide(StreamAudio)
-    stream_cover = provide(StreamCover)
+    stream = provide(Stream)
