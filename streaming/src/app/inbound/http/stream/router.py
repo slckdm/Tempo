624163cd54d918584db8d653909be2773bed889e
Dmitry <zubashenkod@gmail.com>
2026-06-25 17:16:27 +0300

    chore: remove response class, add common responses examples

--- streaming/src/app/inbound/http/stream/router.py
+++ streaming/src/app/inbound/http/stream/router.py
@@ -1,17 +1,41 @@
-from fastapi import APIRouter
-from fastapi.responses import StreamingResponse
+from http import HTTPStatus
 
-from .stream import stream_audio
+from fastapi import APIRouter
+
+from toolkit.service import response
+
+from .stream_audio import stream_audio
 from .stream_cover import stream_cover
 
 
 def make_stream_router() -> APIRouter:
     router = APIRouter(prefix="/stream")
+    common_responses = {
+        HTTPStatus.UNAUTHORIZED: {
+            "model": response.UnauthorizedResponse,
+            "description": "Unauthorized",
+        },
+        HTTPStatus.BAD_REQUEST: {
+            "model": response.BadRequestResponse,
+            "description": "Bad request",
+        },
+        HTTPStatus.FORBIDDEN: {"model": response.ForbiddenResponse, "description": "Forbidden"},
+        HTTPStatus.INTERNAL_SERVER_ERROR: {
+            "model": response.InternalServerErrorResponse,
+            "description": "Internal server error",
+        },
+    }
     router.add_api_route(
-        path="/{id}", endpoint=stream_audio, response_class=StreamingResponse, methods=["GET"]
+        path="/{id}",
+        endpoint=stream_audio,
+        methods=["GET"],
+        responses={**common_responses},
     )
     router.add_api_route(
-        path="/{id}/cover", endpoint=stream_cover, response_class=StreamingResponse, methods=["GET"]
+        path="/{id}/cover",
+        endpoint=stream_cover,
+        methods=["GET"],
+        responses={**common_responses},
     )
 
     return router
