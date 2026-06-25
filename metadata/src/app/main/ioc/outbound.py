77ff5707cceec879d62f73954b1e82668c3fd2e0
Dmitry <zubashenkod@gmail.com>
2026-06-25 17:06:27 +0300

    chore: move outbound providers

---
+++ metadata/src/app/main/ioc/outbound.py
@@ -0,0 +1,75 @@
+from collections.abc import AsyncIterator
+from typing import Sequence
+
+from dishka import Provider, Scope, provide
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from toolkit.clients import KeycloakClient, KeycloakConfig
+from toolkit.s3 import S3Client
+
+from app.main.config.loader import PostgresSettings
+from app.main.config.settings import KeycloakSettings, S3Settings
+
+
+class KeycloakClientProvider(Provider):
+    @provide(scope=Scope.REQUEST)
+    def new_client(self, config: KeycloakSettings) -> KeycloakClient:
+        return KeycloakClient(
+            KeycloakConfig(
+                url=config.URL,
+                realm=config.REALM,
+                client_id=config.CLIENT_ID,
+                client_secret=config.CLIENT_SECRET,
+                token_url=config.token_url,
+            )
+        )
+
+
+class PostgresProvider(Provider):
+    @provide(scope=Scope.APP)
+    async def provide_async_engine(
+        self,
+        postgres: PostgresSettings,
+    ) -> AsyncIterator[AsyncEngine]:
+        engine = create_async_engine(postgres.dsn)
+        yield engine
+        await engine.dispose()
+
+    @provide(scope=Scope.APP)
+    async def provide_async_sessionfactory(
+        self, engine: AsyncEngine
+    ) -> async_sessionmaker[AsyncSession]:
+        return async_sessionmaker(bind=engine, expire_on_commit=False)
+
+    @provide(scope=Scope.REQUEST)
+    async def provide_async_session(
+        self, factory: async_sessionmaker[AsyncSession]
+    ) -> AsyncIterator[AsyncSession]:
+        async with factory() as session:
+            yield session
+
+
+class S3Provider(Provider):
+    scope = Scope.REQUEST
+
+    @provide
+    def get_client(self, config: S3Settings) -> S3Client:
+        return S3Client(
+            region_name=config.REGION_NAME,
+            url=config.URL,
+            access_key_id=config.ACCESS_KEY,
+            secret_access_key=config.SECRET_KEY,
+        )
+
+
+def get_outbound_providers() -> Sequence[Provider]:
+    return (
+        KeycloakClientProvider(),
+        PostgresProvider(),
+        S3Provider(),
+    )
