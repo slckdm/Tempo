ac6b26b27cafb86d9904089c27f98c2ab0a7f01b
Dmitry <zubashenkod@gmail.com>
2026-06-25 17:15:53 +0300

    chore: add cover param

--- streaming/src/app/inbound/http/stream/stream_cover.py
+++ streaming/src/app/inbound/http/stream/stream_cover.py
@@ -7,15 +7,15 @@ from dishka.integrations.fastapi import FromDishka, inject
 
 from toolkit.types.urn import UploadURNType
 
-from app.core.commands.stream_cover import StreamCover
+from app.core.queries.stream import Stream
 
 
 @inject
 async def stream_cover(
-    request: Request, id: UploadURNType, interactor: FromDishka[StreamCover]
+    request: Request, id: UploadURNType, interactor: FromDishka[Stream]
 ) -> StreamingResponse:
     """Create file upload."""
-    stream_data = await interactor(id, request.headers.get("range"))
+    stream_data = await interactor(id, request.headers.get("range"), cover=True)
     headers = {"Accept-Ranges": "bytes", "Content-Length": str(stream_data.content_length)}
     if stream_data.content_range:
         headers.update({"Content-Range": str(stream_data.content_range)})
