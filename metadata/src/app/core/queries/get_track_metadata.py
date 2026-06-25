63ab9b674b488f3e54e6183a50f7801f54572434
Dmitry <zubashenkod@gmail.com>
2026-06-25 17:01:38 +0300

    chore: move commands to queries

---
+++ metadata/src/app/core/queries/get_track_metadata.py
@@ -0,0 +1,24 @@
+from uuid import UUID
+
+from toolkit.service.exceptions import NotFoundException
+
+from app.core.common.services.current_user_service import CurrentUserService
+from app.core.queries.models.metadata import MetadataQM
+from app.core.queries.ports.metadata_reader import MetadataReader
+
+
+class GetTrackMetadata:
+    def __init__(
+        self,
+        metadata_reader: MetadataReader,
+        current_user_service: CurrentUserService,
+    ) -> None:
+        self._metadata_reader = metadata_reader
+        self._current_user_service = current_user_service
+
+    async def __call__(self, upload_id: UUID) -> MetadataQM:
+        await self._current_user_service.get_current_user()
+        metadata = await self._metadata_reader.get_by_id(upload_id)
+        if not metadata:
+            raise NotFoundException
+        return metadata
---
+++ metadata/src/app/core/queries/get_tracks_metadata.py
@@ -0,0 +1,23 @@
+from app.core.common.services.current_user_service import CurrentUserService
+from app.core.common.services.metadata_service import MetadataService
+from app.core.queries.models.list_metadata import ListMetadataQM
+from app.core.queries.ports.metadata_reader import FilterParams, MetadataReader
+from app.core.queries.schemas.pagination import PaginationParams
+
+
+class GetTracksMetadata:
+    def __init__(
+        self,
+        metadata_service: MetadataService,
+        metadata_reader: MetadataReader,
+        current_user_service: CurrentUserService,
+    ) -> None:
+        self._metadata_service = metadata_service
+        self._metadata_reader = metadata_reader
+        self._current_user_service = current_user_service
+
+    async def __call__(
+        self, filters: FilterParams, pagination: PaginationParams
+    ) -> ListMetadataQM:
+        await self._current_user_service.get_current_user()
+        return await self._metadata_reader.list_by_filter(filters, pagination)
