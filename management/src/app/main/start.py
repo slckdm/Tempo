84ae6f6e3a7867f15af5c7d8156df3f7c203a79a
Dmitry <zubashenkod@gmail.com>
2026-06-25 16:58:26 +0300

    chore: replace handlers and update imports

--- management/src/app/main/start.py
+++ management/src/app/main/start.py
@@ -11,15 +11,15 @@ from dishka.integrations.fastapi import FastapiProvider, setup_dishka
 from toolkit.s3.s3_client import NoSuchKeyException
 from toolkit.service.exceptions import ForbiddenException, UnauthorizedException
 
-from app.core.common import handlers
+from app.core.common.jsend_error_handler import JsendErrorHandler, JsendFailHandler
 from app.inbound.http.uploads.router import make_uploads_router
 from app.main.config.loader import (
     load_app_settings,
     load_keycloak_settings,
     load_postgres_settings,
+    load_redis_settings,
     load_s3_settings,
     load_sqlalchemy_settings,
-    load_redis_settings,
 )
 from app.main.config.settings import (
     AppSettings,
@@ -30,7 +30,6 @@ from app.main.config.settings import (
     SQLAlchemySettings,
 )
 from app.main.ioc.provider_registry import get_providers
-from app.outbound.keycloak_client_provider import KeycloakClientProvider
 from app.outbound.providers import get_outbound_providers
 
 
@@ -49,15 +48,17 @@ def create_service() -> FastAPI:
         debug=app_settings.DEBUG,
         title=app_settings.NAME,
         exception_handlers={
-            HTTPStatus.UNAUTHORIZED: handlers.unauthorized_error_handler,
-            HTTPStatus.FORBIDDEN: handlers.forbidden_error_handler,
-            HTTPStatus.UNSUPPORTED_MEDIA_TYPE: handlers.unsupported_media_error_handler,
-            HTTPStatus.UNPROCESSABLE_CONTENT: handlers.validation_error_handler,
-            HTTPStatus.NOT_FOUND: handlers.not_found_error_handler,
-            HTTPStatus.INTERNAL_SERVER_ERROR: handlers.internal_server_error_handler,
-            UnauthorizedException: handlers.unauthorized_error_handler,
-            ForbiddenException: handlers.forbidden_error_handler,
-            NoSuchKeyException: handlers.no_such_key_error_handler,
+            HTTPStatus.UNAUTHORIZED: JsendErrorHandler(HTTPStatus.UNAUTHORIZED),
+            HTTPStatus.FORBIDDEN: JsendErrorHandler(HTTPStatus.FORBIDDEN),
+            HTTPStatus.UNSUPPORTED_MEDIA_TYPE: JsendErrorHandler(
+                HTTPStatus.UNSUPPORTED_MEDIA_TYPE
+            ),
+            HTTPStatus.UNPROCESSABLE_CONTENT: JsendErrorHandler(HTTPStatus.UNPROCESSABLE_CONTENT),
+            HTTPStatus.NOT_FOUND: JsendErrorHandler(HTTPStatus.NOT_FOUND),
+            HTTPStatus.INTERNAL_SERVER_ERROR: JsendFailHandler(HTTPStatus.INTERNAL_SERVER_ERROR),
+            UnauthorizedException: JsendErrorHandler(HTTPStatus.UNAUTHORIZED),
+            ForbiddenException: JsendErrorHandler(HTTPStatus.FORBIDDEN),
+            NoSuchKeyException: JsendErrorHandler(HTTPStatus.NOT_FOUND),
         },
         routes=[*make_uploads_router().routes],
     )
