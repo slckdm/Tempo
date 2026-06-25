1cdd9fb02f994c03f9a125311324100c9aa9b501
Dmitry <zubashenkod@gmail.com>
2026-06-25 16:55:46 +0300

    chore: split base and http exception

--- toolkit/toolkit/service/exceptions.py
+++ toolkit/toolkit/service/exceptions.py
@@ -1,26 +1,30 @@
 from http import HTTPStatus
 
 
-class TempoException(Exception):
+class TempoBaseException(Exception):
+    message: str | None = None
+
+    def __init__(self, message: str | None = None) -> None:
+        self.message = message or self.__class__.message
+        super().__init__(message) if message else super().__init__()
+
+
+class TempoHTTPException(TempoBaseException):
     status_code: HTTPStatus | int
-    detail: str
     data: dict
 
-    def __init__(self, detail: str | None = None, data: dict | None = None) -> None:
-        self.detail = detail or self.__class__.detail
+    def __init__(self, message: str | None = None, data: dict | None = None) -> None:
+        super().__init__(message=message)
         self.data = data or {}
 
 
-class UnauthorizedException(TempoException):
-    status_code = HTTPStatus.UNAUTHORIZED
-    detail = HTTPStatus.UNAUTHORIZED.phrase
+class UnauthorizedException(TempoBaseException):
+    ...
 
 
-class ForbiddenException(TempoException):
-    status_code = HTTPStatus.FORBIDDEN
-    detail = HTTPStatus.FORBIDDEN.phrase
+class ForbiddenException(TempoBaseException):
+    ...
 
 
-class NotFoundException(TempoException):
-    status_code = HTTPStatus.NOT_FOUND
-    detail = HTTPStatus.NOT_FOUND.phrase
+class NotFoundException(TempoBaseException):
+    ...
