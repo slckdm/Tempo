9aea79c0b8f20e615f3fdb4faba70f01c30e2aed
Dmitry <zubashenkod@gmail.com>
2026-06-26 22:14:59 +0300

    tests: add finish/complete upload tests

---
+++ management/tests/unit/core/commands/test_complete_upload.py
@@ -0,0 +1,78 @@
+import pytest
+from faker import Faker
+from toolkit.types.enum import UploadStatus
+
+from app.core.commands.complete_upload import CompleteUpload
+from app.core.commands.ports.flusher import Flusher
+from app.core.commands.ports.object_storage import ObjectStorage
+from app.core.commands.ports.outbox_storage import OutboxStorage
+from app.core.commands.ports.transaction import Transaction
+from app.core.commands.ports.upload_storage import UploadStorage
+from app.core.common.ports.auth_user_finder import AuthorizedUserFinder
+from app.core.common.ports.identity_provider import IdentityProvider
+from app.core.common.ports.utc_timer import UTCTimer
+from app.core.common.services.current_user_service import CurrentUserService
+from app.core.common.services.outbox_service import OutboxService
+from app.core.common.services.upload_service import UploadService
+from tests.unit.core.factories import (
+    create_current_user_service,
+    create_outbox_service,
+    create_upload,
+    create_upload_service,
+)
+
+
+def make_complete_upload_command(
+    current_user_service: CurrentUserService,
+    upload_service: UploadService,
+    upload_storage: UploadStorage,
+    outbox_service: OutboxService,
+    outbox_storage: OutboxStorage,
+    object_storage: ObjectStorage,
+    flusher: Flusher,
+    transaction: Transaction,
+) -> CompleteUpload:
+    return CompleteUpload(
+        current_user_service=current_user_service,
+        upload_service=upload_service,
+        upload_storage=upload_storage,
+        outbox_service=outbox_service,
+        outbox_storage=outbox_storage,
+        object_storage=object_storage,
+        flusher=flusher,
+        transaction=transaction,
+    )
+
+
+@pytest.mark.asyncio
+async def test_complete_upload_success(
+    identity_provider: IdentityProvider,
+    authorized_user_finder: AuthorizedUserFinder,
+    utc_timer: UTCTimer,
+    upload_storage: UploadStorage,
+    outbox_storage: OutboxStorage,
+    object_storage: ObjectStorage,
+    flusher: Flusher,
+    transaction: Transaction,
+) -> None:
+    upload = create_upload(status=UploadStatus.PENDING)
+    upload_storage.get_by_id.return_value = upload
+    complete_upload_command = make_complete_upload_command(
+        current_user_service=create_current_user_service(
+            identity_provider, authorized_user_finder
+        ),
+        upload_service=create_upload_service(),
+        outbox_service=create_outbox_service(utc_timer),
+        upload_storage=upload_storage,
+        outbox_storage=outbox_storage,
+        object_storage=object_storage,
+        flusher=flusher,
+        transaction=transaction,
+    )
+    response = await complete_upload_command(upload.urn)
+
+    upload_storage.get_by_id.assert_called_once_with(upload.id, True)
+    object_storage.get_object.assert_called_once_with(str(upload.urn))
+    outbox_storage.add.assert_called_once()
+    flusher.flush.assert_called_once()
+    transaction.commit.assert_called_once()
---
+++ management/tests/unit/core/commands/test_finish_upload.py
@@ -0,0 +1,50 @@
+import pytest
+from toolkit.messaging.contracts import MetadataReadyEvent
+from toolkit.types.enum import UploadStatus
+
+from app.core.commands.finish_upload import FinishUpload
+from app.core.commands.ports.flusher import Flusher
+from app.core.commands.ports.transaction import Transaction
+from app.core.commands.ports.upload_storage import UploadStorage
+from app.core.common.services.upload_service import UploadService
+from tests.unit.core.factories import (
+    create_upload,
+    create_upload_service,
+)
+
+
+def make_finish_upload_command(
+    upload_service: UploadService,
+    upload_storage: UploadStorage,
+    flusher: Flusher,
+    transaction: Transaction,
+) -> FinishUpload:
+    return FinishUpload(
+        upload_service=upload_service,
+        upload_storage=upload_storage,
+        flusher=flusher,
+        transaction=transaction,
+    )
+
+
+@pytest.mark.asyncio
+async def test_complete_upload_success(
+    upload_storage: UploadStorage,
+    flusher: Flusher,
+    transaction: Transaction,
+) -> None:
+    upload = create_upload(status=UploadStatus.PROCESSING)
+    upload_storage.get_by_id.return_value = upload
+    command = make_finish_upload_command(
+        upload_service=create_upload_service(),
+        upload_storage=upload_storage,
+        flusher=flusher,
+        transaction=transaction,
+    )
+    payload = MetadataReadyEvent(upload_id=upload.urn, cover_key="dsf")
+
+    await command(payload)
+
+    upload_storage.get_by_id.assert_called_once_with(upload.urn.id)
+    flusher.flush.assert_called_once()
+    transaction.commit.assert_called_once()
