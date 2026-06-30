0b744912fe5e699bca9cbb14468cd7060de92851
Dmitry <zubashenkod@gmail.com>
2026-07-01 01:30:23 +0300

    fix: wrong param type

--- toolkit/src/toolkit/entities/object.py
+++ toolkit/src/toolkit/entities/object.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, ConfigDict, Field, SkipValidation
 class Object(BaseModel):
 
     body: SkipValidation[IOBase] = Field(alias="Body")
-    content_range: int | None = Field(None, alias="ContentRange")
+    content_range: str | None = Field(None, alias="ContentRange")
     content_length: int = Field(alias="ContentLength")
     content_type: str = Field(alias="ContentType")
 
