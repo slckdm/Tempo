25c1ee159c8d24148c8316cd1dc0262423cb3f30
Dmitry <zubashenkod@gmail.com>
2026-07-02 15:23:47 +0300

    chore: update import

--- management/tests/unit/core/common/services/test_upload_service.py
+++ management/tests/unit/core/common/services/test_upload_service.py
@@ -4,8 +4,7 @@ from toolkit.types.enum import UploadStatus
 
 from app.core.common.exceptions import StatusUpdateFlowError
 from app.core.models import Upload
-
-from ...factories import create_upload, create_upload_service, create_user
+from tests.unit.core.factories import create_upload, create_upload_service, create_user
 
 
 @pytest.mark.asyncio
