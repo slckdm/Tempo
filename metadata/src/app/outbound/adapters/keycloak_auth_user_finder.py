ef553d6c58f68928a1a0348fdccc5d70c2f408ef
Dmitry <zubashenkod@gmail.com>
2026-07-07 20:46:17 +0300

    chore: rename variable

--- metadata/src/app/outbound/adapters/keycloak_auth_user_finder.py
+++ metadata/src/app/outbound/adapters/keycloak_auth_user_finder.py
@@ -1,24 +1,25 @@
 from toolkit.clients import KeycloakClient
-from toolkit.entities import ServiceAccount, User
+from toolkit.entities import User
 from toolkit.types_ import UserID
 
 from app.core.common.ports.auth_user_finder import AuthorizedUserFinder
 
 
 class KeycloakAuthorizedUserFinder(AuthorizedUserFinder):
+
     def __init__(self, client: KeycloakClient) -> None:
         self._client = client
 
-    async def get_by_id(self, id: UserID) -> User | ServiceAccount | None:
-        user = await self._client.get_user_by_id(str(id))
+    async def get_by_id(self, id: UserID) -> User | None:
+        user_data = await self._client.get_user_by_id(str(id))
 
-        if not user:
+        if not user_data:
             return None
 
         return User(
-            id=user["id"],
-            username=user["username"],
-            email=user["email"],
-            first_name=user["firstName"],
-            last_name=user["lastName"],
+            id=user_data["id"],
+            username=user_data["username"],
+            email=user_data["email"],
+            first_name=user_data["firstName"],
+            last_name=user_data["lastName"],
         )
