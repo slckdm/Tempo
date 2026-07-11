4c78f857c086a474143d1d0aa5bf33685e8560f4
Dmitry <zubashenkod@gmail.com>
2026-07-12 01:24:58 +0300

    chore: add test user factorites

--- streaming/tests/unit/core/factories.py
+++ streaming/tests/unit/core/factories.py
@@ -11,8 +11,10 @@ from tempo_toolkit.application.auth import (
     AuthorizedUserFinder,
     CurrentUserService,
     IdentityProvider,
+    User,
 )
 from tempo_toolkit.application.storage import ObjectStorage, StoredObject
+from tempo_toolkit.contracts.identifiers import UserID
 from tempo_toolkit.contracts.uploads import UploadURN
 from tempo_toolkit.infrastructure.web import FastAPITokenProvider
 
@@ -58,4 +60,40 @@ def create_current_user_service(
     return CurrentUserService(
         identity_provider=identity_provider,
         authorized_user_finder=authorized_user_finder,
+    )
+
+
+def create_user_id(value: UserID | None = None) -> UserID:
+    return value or UserID(uuid4())
+
+
+def create_username(value: str | None = None) -> str:
+    return value or Faker().user_name()
+
+
+def create_first_name(value: str | None = None) -> str:
+    return value or Faker().first_name()
+
+
+def create_last_name(value: str | None = None) -> str:
+    return value or Faker().last_name()
+
+
+def create_email(value: str | None = None) -> str:
+    return value or Faker().email()
+
+
+def create_user(
+    id: UserID | None = None,
+    username: str | None = None,
+    first_name: str | None = None,
+    last_name: str | None = None,
+    email: str | None = None
+) -> User:
+    return User(
+        id=id or create_user_id(),
+        username=username or create_username(),
+        first_name=first_name or create_first_name(),
+        last_name=last_name or create_last_name(),
+        email=email or create_email()
     )
