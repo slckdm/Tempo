bbf43ef786d91ca68821cd90959f9266aba39045
Dmitry <zubashenkod@gmail.com>
2026-07-15 21:06:19 +0300

    docs: add comments

--- toolkit/packages/contracts/src/tempo_toolkit/contracts/routing.py
+++ toolkit/packages/contracts/src/tempo_toolkit/contracts/routing.py
@@ -15,12 +15,15 @@ class RoutingKey:
         return f"{self.prefix}.{self.route}"
 
 
-UPLOAD_CREATED_RK = RoutingKey("tempo.mng.upload.created")
-UPLOAD_COMPLETED_RK = RoutingKey("tempo.mng.upload.completed")
-UPLOAD_DELETED_RK = RoutingKey("tempo.mng.upload.deleted")
-METADATA_READY_RK = RoutingKey("tempo.md.metadata.ready")
-METADATA_FAILED_RK = RoutingKey("tempo.md.metadata.failed")
-METADATA_DELETED_RK = RoutingKey("tempo.md.metadata.deleted")
+# Events RK
+# - Management
+UPLOAD_CREATED_EVENT_RK = RoutingKey("tempo.mng.upload.created")
+UPLOAD_COMPLETED_EVENT_RK = RoutingKey("tempo.mng.upload.completed")
+UPLOAD_DELETED_EVENT_RK = RoutingKey("tempo.mng.upload.deleted")
+# - Metadata
+METADATA_READY_EVENT_RK = RoutingKey("tempo.md.metadata.ready")
+METADATA_FAILED_EVENT_RK = RoutingKey("tempo.md.metadata.failed")
+METADATA_DELETED_EVENT_RK = RoutingKey("tempo.md.metadata.deleted")
 # Commands RK
 # - Metadata
 METADATA_PROCESS_METADATA_RK = RoutingKey("tempo.md.metadata.process_metadata")
