6e8a01d4b6f4bb7c5b735d02b1b279373e590b66
Dmitry <zubashenkod@gmail.com>
2026-07-15 21:11:57 +0300

    chore: add event factories for upload aggregate

--- management/src/app/core/common/services/upload_service.py
+++ management/src/app/core/common/services/upload_service.py
@@ -3,16 +3,33 @@ from mimetypes import guess_type
 
 from tempo_toolkit.application.auth import User
 from tempo_toolkit.application.errors import UnsupportedMediaType
+from tempo_toolkit.application.outbox import OutboxService, OutboxStorage
+from tempo_toolkit.contracts.routing import (
+    UPLOAD_COMPLETED_EVENT_RK,
+    UPLOAD_CREATED_EVENT_RK,
+    UPLOAD_DELETED_EVENT_RK,
+)
 from tempo_toolkit.contracts.uploads import UploadStatus
 
+from app.core.common.enums.aggregate_type import AggregateType
 from app.core.common.exceptions import StatusUpdateFlowError
 from app.core.common.factories.id_factory import generate_upload_id
+from app.core.common.factories.message_factorites import (
+    make_upload_completed_message,
+    make_upload_created_message,
+    make_upload_deleted_message,
+)
 from app.core.models.upload import Upload
 
 
 class UploadService:
-
-    def __init__(self): ...
+    def __init__(
+        self,
+        outbox_service: OutboxService,
+        outbox_storage: OutboxStorage,
+    ) -> None:
+        self._outbox_service = outbox_service
+        self._outbox_storage = outbox_storage
 
     async def create_upload(
         self, filename: str, size: int, user: User
@@ -34,6 +51,9 @@ class UploadService:
 
         return upload
 
+    async def complete_upload(self, upload: Upload) -> None:
+        await self.transit_status(upload, UploadStatus.PROCESSING)
+
     async def transit_status(self, upload: Upload, status: UploadStatus) -> None:
         TRANSITIONS_MAP = {
             UploadStatus.PROCESSING: (UploadStatus.PENDING,),
@@ -45,5 +65,32 @@ class UploadService:
             upload.status = status
         else:
             raise StatusUpdateFlowError
+
+    async def make_upload_completed_event(self, upload: Upload) -> None:
+        message = await self._outbox_service.create_message(
+            aggregate_type=AggregateType.UPLOAD,
+            aggregate_id=str(upload.id),
+            event_type=UPLOAD_COMPLETED_EVENT_RK,
+            payload=make_upload_completed_message(upload),
+        )
+        await self._outbox_storage.add(message)
+
+    async def make_upload_created_event(self, upload: Upload) -> None:
+        message = await self._outbox_service.create_message(
+            aggregate_type=AggregateType.UPLOAD,
+            aggregate_id=str(upload.id),
+            event_type=UPLOAD_CREATED_EVENT_RK,
+            payload=make_upload_created_message(upload),
+        )
+        await self._outbox_storage.add(message)
+
+    async def make_upload_deleted_event(self, upload: Upload) -> None:
+        message = await self._outbox_service.create_message(
+            aggregate_type=AggregateType.UPLOAD,
+            aggregate_id=str(upload.id),
+            event_type=UPLOAD_DELETED_EVENT_RK,
+            payload=make_upload_deleted_message(upload),
+        )
+        await self._outbox_storage.add(message)
 
     def list_uploads(self) -> ...: ...
