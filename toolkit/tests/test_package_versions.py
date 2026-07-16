e23e2ae0dd581afade717bedbdd413223f80f050
Dmitry <zubashenkod@gmail.com>
2026-07-16 18:44:23 +0300

    chore: bump tempo toolkit packages versions

--- toolkit/tests/test_package_versions.py
+++ toolkit/tests/test_package_versions.py
@@ -5,4 +5,4 @@ from scripts.check_versions import PACKAGE_NAMES, read_version
 
 def test_package_versions_are_synchronized() -> None:
     """All toolkit distributions use one release version."""
-    assert {read_version(package_name) for package_name in PACKAGE_NAMES} == {"0.2.0"}
+    assert {read_version(package_name) for package_name in PACKAGE_NAMES} == {"0.2.1"}
