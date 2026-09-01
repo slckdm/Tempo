4c0278a39440f530777dcaf00a0f4c15057c4f74
Dmitry <zubashenkod@gmail.com>
2026-09-01 21:35:46 +0300

    chore: add metadata deleted event

--- metadata/src/app/core/commands/delete_track_metadata.py
+++ metadata/src/app/core/commands/delete_track_metadata.py
@@ -1,10 +1,13 @@
-from tempo_toolkit.application.outbox import OutboxService, OutboxStorage
+from tempo_toolkit.application.outbox import OutboxMessage, OutboxService, OutboxStorage
 from tempo_toolkit.application.persistence import Flusher, Transaction
 from tempo_toolkit.application.storage import ObjectStorage
-from tempo_toolkit.contracts.events import UploadDeletedEvent
+from tempo_toolkit.contracts.events import MetadataDeletedEvent, UploadDeletedEvent
+from tempo_toolkit.contracts.routing import METADATA_DELETED_EVENT_RK
 
 from app.core.commands.ports.metadata_storage import MetadataStorage
+from app.core.common.enums.aggregate_type import AggregateType
 from app.core.common.services.metadata_service import MetadataService
+from app.core.models.track_metadata import TrackMetadata
 
 
 class DeleteTrackMetadata:
@@ -35,5 +38,18 @@ class DeleteTrackMetadata:
             await self._object_storage.delete_object(metadata.cover_key)
         await self._metadata_storage.delete(metadata)
 
+        event = await self._create_notification(metadata, payload)
+        await self._outbox_storage.add(event)
+
         await self._flusher.flush()
         await self._transaction.commit()
+
+    async def _create_notification(
+        self, metadata: TrackMetadata, payload: UploadDeletedEvent
+    ) -> OutboxMessage:
+        return await self._outbox_service.create_message(
+            aggregate_type=AggregateType.METADATA,
+            aggregate_id=str(metadata.upload_id),
+            event_type=METADATA_DELETED_EVENT_RK,
+            payload=MetadataDeletedEvent(upload_id=payload.upload_id),
+        )
